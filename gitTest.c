abec964567a05c51affc650a4824c900654c6e5a
Jonathan Gustafson <jonathgu@kth.se>
2021-03-02 08:57:29 +0100

    Update gitTest.c
    Lagt till ett förslag till struktur på main funktion.

--- gitTest.c
+++ gitTest.c
@@ -1,8 +1,27 @@
+/*****************************************************
+Lite test för hur vi skulle använda github och
+hur strukturen för spellogiken kan se ut
+*****************************************************/
 <#include <stdio.h>
+
+void play();
+void gameOver();
+void menu();
 
 int main(){
 
+  while(1){
 
+    //Meny
+    menu();
+
+    //Spela spelet
+    play();
+
+    //Gameover, gå till menyn
+    gameOver();
+
+  }
 
   return 0;
 }
