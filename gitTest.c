1387e53ea9ba4f9e3e758dcd1841f38274335c18
Jonathan Gustafson <jonathgu@kth.se>
2021-03-02 09:21:54 +0100

    Update gitTest.c
    Har testat att bygga en liten bas för hur spelet kan byggas upp.

--- gitTest.c
+++ gitTest.c
@@ -1,24 +1,62 @@
 /*****************************************************
 Lite test för hur vi skulle använda github och
 hur strukturen för spellogiken kan se ut
+-----------------------------------------------------
+Struktur skriven av Jonathan Gustafson
 *****************************************************/
 <#include <stdio.h>
 
+void menu();
 void play();
 void gameOver();
-void menu();
+
+void menu(){
+  while(1){
+
+  //Show start screen
+
+  //If(start_btn) -> return;
+
+  }
+}
+
+void play(){
+
+  //Draw the tubes and the bird
+
+  //Countdown 3, 2, 1, GO
+
+  while(1){
+    //Move tubes to the left, keep spawning new tubes with random lengths
+
+    //if(jump_btn) -> jump();
+
+    //if (bird_cordinates == tube_cordinates) -> return;
+  }
+}
+
+void gameOver(){
+  while(1){
+
+  //Show gameover screen
+
+  //If(start_btn) -> return;
+
+  }
+}
 
 int main(){
 
+  //infinite loop to run the program
   while(1){
 
-    //Meny
+    //Menu
     menu();
 
-    //Spela spelet
+    //Start the game
     play();
 
-    //Gameover, gå till menyn
+    //Gameover, go to menu
     gameOver();
 
   }
